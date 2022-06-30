a086b25e7c50a932d27b6886758fc0455f6de765
pavanreddy <pavanrmacha@gmail.com>
2022-06-30 13:21:28 +0530

    expence Tracker in crud crud

--- expenseincrudcrud.js
+++ expenseincrudcrud.js
@@ -16,7 +16,7 @@ function onSubmit(e) {
         Ecategory: Ecategory.value
     }
     //storing data on the cloud
-    axios.post("https://crudcrud.com/api/afd2382566d945efb9fd8c9ccf3b3de9/ExpData", ExpenseDetails)
+    axios.post("https://crudcrud.com/api/6e05916f8b11416cbb2edc1e01857e76/ExpData", ExpenseDetails)
         .then((response) => {
             showExpenseOnScreen(response.data)
             console.log(response);
@@ -26,7 +26,7 @@ function onSubmit(e) {
         })
 }
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/afd2382566d945efb9fd8c9ccf3b3de9/ExpData")
+    axios.get("https://crudcrud.com/api/6e05916f8b11416cbb2edc1e01857e76/ExpData")
         .then((response) => {
             console.log(response)
             for (var i = 0; i < response.data.length; i++) {
@@ -37,17 +37,18 @@ window.addEventListener("DOMContentLoaded", () => {
         })
 })
 function showExpenseOnScreen(Expense) {
+    console.log(Expense)
     if (localStorage.getItem(Expense.Eamount) !== null) {
         removeExpenseFromTheScreen(Expense.Eamount)
     }
     const parentNode = document.getElementById('listOfExpense');
     const childHTML = `<li id="${Expense._id}" > ${Expense.Eamount} -  ${Expense.Edescription} -${Expense.Ecategory}
             <button  onclick=removeExpense('${Expense._id}')>Delete Expense</button>                    
-            <button onclick=editExpense('${Expense.Eamount}','${Expense.Edescription}','${Expense.Ecategory})> Edit Expense</button></li>`;
+            <button onclick=editExpense('${Expense.Eamount}','${Expense.Edescription}','${Expense.Ecategory}','${Expense._id}')> Edit Expense</button></li>`;
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
 }
 function removeExpense(ExpenseId) {
-    axios.delete(`https://crudcrud.com/api/afd2382566d945efb9fd8c9ccf3b3de9/ExpData/${ExpenseId}`)
+    axios.delete(`https://crudcrud.com/api/6e05916f8b11416cbb2edc1e01857e76/ExpData/${ExpenseId}`)
         .then((response) => {
             removeExpenseFromTheScreen(ExpenseId)
         })
@@ -55,12 +56,12 @@ function removeExpense(ExpenseId) {
             console.log(err);
         })
 }
-function editExpense(Eamount, Edescription, Ecategory) {
+function editExpense(Eamount, Edescription, Ecategory,ExpenseId) {
     document.getElementById('Expenseamount').value = Eamount
     document.getElementById('description').value = Edescription
     document.getElementById('Category').value = Ecategory
-     //removeExpense(ExpenseId)
-     //removeExpenseFromTheScreen(ExpenseId)
+    removeExpense(ExpenseId)
+     removeExpenseFromTheScreen(ExpenseId)
 }
 function removeExpenseFromTheScreen(ExpenseId) {
     const parentNode = document.getElementById('listOfExpense');
